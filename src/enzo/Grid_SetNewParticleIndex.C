93e427478b6ffa8d1fb16f170b1c98f8725c56c7
Stephen Skory <stephenskory@yahoo.com>
2010-04-13 09:00:04 -0700

    Saving modifications to my Kravstov SM in my branch, and adding new star output to SetNewParticleIndex so I can track exactly where stars are formed.

--- src/enzo/Grid_SetNewParticleIndex.C
+++ src/enzo/Grid_SetNewParticleIndex.C
@@ -25,6 +25,16 @@
 
 void grid::SetNewParticleIndex(int &NumberCount1, PINT &NumberCount2)
 {
+  FILE * fp;
+  char fname[MAX_LINE_LENGTH];
+  sprintf(fname, "NewStars%03d.out", MyProcessorNumber);
+  fp = fopen(fname, "a");
+  
+  // Figure the grid cell size
+  int num = GridDimension[0] - 2*DEFAULT_GHOST_ZONES;
+  float fnum = (float)(num);
+  float cell = (GridRightEdge[0] - GridLeftEdge[0])/fnum;
+  
   int n, abstype;
   for (n = 0; n < NumberOfParticles; n++) 
     if (ParticleNumber[n] == INT_UNDEFINED) {
@@ -35,8 +45,13 @@ void grid::SetNewParticleIndex(int &NumberCount1, PINT &NumberCount2)
 	ParticleNumber[n] = NumberCount1++ + NumberCount2;
       else 
 	ParticleNumber[n] = NumberCount1 + NumberCount2++;
+
+    fprintf(fp, "%d %1.12e %1.12e %1.12e %1.12e %1.12e\n", ParticleNumber[n], Time,
+    cell, ParticlePosition[0][n], ParticlePosition[1][n], ParticlePosition[2][n]);
+
 //      printf("New star particle index = %d (%d %d)\n",
 //	     ParticleNumber[n], NumberCount1, NumberCount2);
     }
+  fclose(fp);
   return;
 }
