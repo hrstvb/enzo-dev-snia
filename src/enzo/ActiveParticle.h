3b120614d07a8c4da9ac1cf8ded543c9f6b2996a
Matthew Turk <matthewturk@gmail.com>
2011-05-13 15:54:48 -0400

    In the group we sketched these out as a first pass at what should be in the base class.

--- src/enzo/ActiveParticle.h
+++ src/enzo/ActiveParticle.h
@@ -30,10 +30,71 @@ class ActiveParticleType
 
     /* This should return the number of new star particles created, and should
      * create them. */
+    ActiveParticleType();
+    ActiveParticleType(grid *_grid, int _id, int _level);
+    ActiveParticle(StarBuffer *buffer, int n);
+    ActiveParticle(StarBuffer buffer) ;
+    ~ActiveParticle();
+    ActiveParticle* copy(void);
+
+  int   ReturnID(void) { return Identifier; };
+  double ReturnMass(void) { return Mass; };
+  float ReturnBirthTime(void) { return BirthTime; };
+
+  int   ReturnLevel(void) { return level; };
+  void  ReduceLevel(void) { level--; };
+  void  IncreaseLevel(void) { level++; };
+  void  SetLevel(int i) { level = i; };
+  void  SetGridID(int i) { GridID = i; };
+  grid *ReturnCurrentGrid(void) { return CurrentGrid; };
+  void  AssignCurrentGrid(grid *a) { this->CurrentGrid = a; };
+  void  AddMass(double dM) { Mass += dM; };
+
+  FLOAT *ReturnPosition(void) { return pos; }
+  float *ReturnVelocity(void) { return vel; }
+  void	ConvertMassToSolar(void);
+  void	Merge(Star a);
+  void	Merge(Star *a);
+  bool	Mergable(Star a);
+  bool  Mergable(Star *a);
+  float Separation(Star a);
+  float Separation(Star *a);
+  float Separation2(Star a);
+  float Separation2(Star *a);
+  float RelativeVelocity2(Star a);
+  float RelativeVelocity2(Star *a);
+  void  UpdatePositionVelocity(void);
+  void	DeleteCopyInGrid(void);
+  int   DeleteCopyInGridGlobal(LevelHierarchyEntry *LevelArray[]);
+  void	CopyToGrid(void);
+  void  MirrorToParticle(void);
+  virtual bool  IsARadiationSource(FLOAT Time) { return FALSE };
+  int   DeleteParticle(LevelHierarchyEntry *LevelArray[]);
+  int   DisableParticle(LevelHierarchyEntry *LevelArray[]);
+  void  ActivateNewStar(FLOAT Time, float Timestep);
+
+  int SphereContained(LevelHierarchyEntry *LevelArray[], int level, 
+		      float Radius);
+
+  ActiveParticle* StarBufferToList(StarBuffer *buffer, int n);
+  StarBuffer* StarListToBuffer(int n);
     
   protected:
+    grid		*CurrentGrid;
+    FLOAT	 pos[MAX_DIMENSION];
+    float		 vel[MAX_DIMENSION];
+    double       	 Mass;		// Msun
+    float		 BirthTime;
+    PINT		 Identifier;
+    int		 level;
+    int		 GridID;
+    star_type	 type;
 
-  private:
+    bool Active;
+
+  friend class grid;
+
+  private: /* Cannot be accessed by subclasses! */
   
 };
 
