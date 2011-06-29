6c16ed9d9705b4f6f12cce76094ec8591f23fe93
dcollins4096 <dcollins4096@localhost>
2011-06-29 12:12:15 -0600

    mhgctmerge: wrote a L1 norm on face centered magnetic field.  Temporary code.

--- src/enzo/Grid_ExtraFunction.C
+++ src/enzo/Grid_ExtraFunction.C
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include "ErrorExceptions.h"
+#include "math.h"
 #include "performance.h"
 #include "macros_and_parameters.h"
 #include "typedefs.h"
@@ -14,8 +15,26 @@
 #include "ExternalBoundary.h"
 #include "Grid.h"
 #include "fortran.def"
-
+static float LastB[] = {0.0,0.0,0.0};
 int grid::ExtraFunction(char * message){
+    if( MyProcessorNumber != ProcessorNumber ){
+        return SUCCESS;
+    }
+    int size=0;
+    float total=0;
+    fprintf(stderr,"MagField %s",message);
+    for(int field=0;field<3;field++){
+        size =MagneticDims[field][0]*MagneticDims[field][1]*MagneticDims[field][2];
+        total=1;
+
+        for(int index=0;index<size; index++){
+            total += fabs(MagneticField[field][index]);
+        }
+        fprintf(stderr," %"FSYM,total-LastB[field]);
+        LastB[field]=total;
+    }
+    fprintf(stderr,"\n");
 
     return SUCCESS;
+
 }
