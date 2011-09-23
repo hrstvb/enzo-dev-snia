bcdcdda5d78798bc890fb2b931c161f2b639b46e
tabel <tabel@localhost>
2011-09-23 09:29:28 -0700

    Added AllocateNewBayonField allocator and associated Free routine.

---
+++ src/enzo/AllocateNewBaryonField.C
@@ -0,0 +1,37 @@
+/***********************************************************************
+/
+/  function AllocateNewBaryonField
+/
+/  written by: Tom Abel
+/  date:       September 2011
+/  modified:   
+/
+/  PURPOSE: Allocates space for the baryons. 
+/           This can use the memory pool. 
+/
+************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "macros_and_parameters.h"
+#include "typedefs.h"
+#include "global_data.h"
+void *AllocateNewBaryonField(int size)
+{
+#ifndef MEMORY_POOL
+  return new float[size];
+#else
+  return BaryonFieldMemoryPool->GetMemory(sizeof(float)*size);
+#endif
+}
+
+void FreeBaryonFieldMemory(float *BF)
+{
+#ifndef MEMORY_POOL
+  delete [] BF;
+#else
+  return BaryonFieldMemoryPool->FreeMemory(BF);
+#endif
+  return;
+}
