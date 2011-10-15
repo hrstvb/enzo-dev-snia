bab6a4e8d4757c4940940648a827b135687f6fd9
John Wise <jwise@physics.gatech.edu>
2011-10-15 12:22:48 -0400

    S&H active particle compiles.  Removing libconfig (with the permission from Matt!)

--- src/enzo/ActiveParticle_SpringelHernquist.C
+++ src/enzo/ActiveParticle_SpringelHernquist.C
@@ -39,7 +39,7 @@ class ActiveParticleType_SpringelHernquist;
 class SpringelHernquistParticleBufferHandler;
 
 class SpringelHernquistGrid : private grid {
-    friend class ActiveParticleType_SpringelHernquist;
+  friend class ActiveParticleType_SpringelHernquist;
 };
 
 /* Note that we only refer to SampleParticleGrid here. 
@@ -59,21 +59,31 @@ public:
 
   // Pop III specific active particle parameters
   static float OverDensityThreshold, PhysicalDensityThreshold;
+  static float MinimumDynamicalTime, MinimumMass;
 
 private:
   float Metallicity;
 };
 
-static int ActiveParticleType_SpringelHernquist::InitializeParticleType() {
+float ActiveParticleType_SpringelHernquist::OverDensityThreshold = FLOAT_UNDEFINED;
+float ActiveParticleType_SpringelHernquist::PhysicalDensityThreshold = FLOAT_UNDEFINED;
+float ActiveParticleType_SpringelHernquist::MinimumDynamicalTime = FLOAT_UNDEFINED;
+float ActiveParticleType_SpringelHernquist::MinimumMass = FLOAT_UNDEFINED;
+
+int ActiveParticleType_SpringelHernquist::InitializeParticleType() {
   // get some parameters from the Param object
 
 #ifdef NEW_CONFIG
   Param.GetScalar(OverDensityThreshold, "Physics.ActiveParticles.SpringelHernquist.OverDensityThreshold");
   Param.GetScalar(PhysicalDensityThreshold, "Physics.ActiveParticles.SpringelHernquist.PhysicalDensityThreshold");
+  Param.GetScalar(MinimumDynamicalTime, "Physics.ActiveParticles.SpringelHernquist.MinimumDynamicalTime");
+  Param.GetScalar(MinimumMass, "Physics.ActiveParticles.SpringelHernquist.MinimumMass");
 #else
   // zero for now, just to check compiling
   OverDensityThreshold = 0.0;
   PhysicalDensityThreshold = 0.0;
+  MinimumDynamicalTime = 0.0;
+  MinimumMass = 0.0;
 #endif
 
 }
@@ -87,8 +97,8 @@ int ActiveParticleType_SpringelHernquist::EvaluateFormation
   float bmass, div, dtot, tdyn, tstar;
   int i, j, k, dim, index, offset_y, offset_z;
   int NumberOfNewParticles = 0;
-  float r_float, y, x, pstar, starfraction;
-  float msolar=SolarMass, mproton=mass_h, beta=0.1,
+  float r_float, y, x, pstar, starfraction, usn;
+  float msolar=SolarMass, mproton=mh, beta=0.1,
     sqrtepssn=2.0e24, kb=kboltz;
 
   // Define the energy output from supernovae
@@ -120,9 +130,9 @@ int ActiveParticleType_SpringelHernquist::EvaluateFormation
       index = GRIDINDEX_NOGHOST(tg->GridStartIndex[0], j, k);
       for (i = tg->GridStartIndex[0]; i <= tg->GridEndIndex[0]; i++, index++) {
 
-    // 0. If no more room for particles, quit.
-    if (supp_data.NumberOfNewParticles >=
-        supp_data.MaxNumberOfNewParticles)
+	// 0. If no more room for particles, quit.
+	if (supp_data.NumberOfNewParticles >=
+	    supp_data.MaxNumberOfNewParticles)
           continue;
 
 	// 1. Finest level of refinement
@@ -133,31 +143,32 @@ int ActiveParticleType_SpringelHernquist::EvaluateFormation
 	if (density[index] < OverDensityThreshold)
 	  continue;
 
-    // Calculate star formation timescale. Eq 21.
-    tstar =  31556926.d0 * mintdyn * pow(density[index] * supp_data.DensityUnits / 
-    	PhysicalDensityThreshold, -0.5);
+	// Calculate star formation timescale. Eq 21.
+	tstar =  31556926.0 * MinimumDynamicalTime * 
+	  pow(density[index] * supp_data.DensityUnits / PhysicalDensityThreshold, -0.5);
 
-   /* note: minus sign is because 'coolrate' is actual backwards: when coolrate is 
-    * negative, gas is cooling (which is the opposite of how it's defined in Springel
-    * & Hernquist 2003, MNRAS, 339, 289, eqtn. 16 */
+	/* note: minus sign is because 'coolrate' is actual backwards: when coolrate is 
+	 * negative, gas is cooling (which is the opposite of how it's defined in Springel
+	 * & Hernquist 2003, MNRAS, 339, 289, eqtn. 16 */
 	y = -1.0 * tstar * supp_data.CoolingRate[index] /
-	    (density[index] * supp_data.DensityUnits) /
-    	(beta * usn - (1.0 - beta) * 1.5 *
-    	kb * supp_data.Temperature[index] / 0.6 / mproton);
+	  (density[index] * supp_data.DensityUnits) /
+	  (beta * usn - (1.0 - beta) * 1.5 *
+	   kb * supp_data.Temperature[index] / 0.6 / mproton);
 
 	// Calculate the fraction of mass in cold clouds.
 	if (y <= 0) {
-		x = 0;
+	  x = 0;
 	} else {
-		x = 1. + 1./ 2. / y - sqrt(1. / y + 1. / 4. / y / y) // Eq(18)
+	  x = 1. + 1./ 2. / y - sqrt(1. / y + 1. / 4. / y / y); // Eq(18)
 	}
 
-    // Calculate total baryon mass in the cell.
-    bmass = density[index] * supp_data.MassUnits;
+	// Calculate total baryon mass in the cell.
+	bmass = density[index] * supp_data.MassUnits;
 
-    // Calculate a parameter which is compared to a random number.
-	pstar = (bmass / smthresh) * (1. - exp(-(1. - beta) * 
-		x * dt * supp_data.TimeUnits / tstar)); // Eq(39)
+	// Calculate a parameter which is compared to a random number.
+	pstar = (bmass / MinimumMass) * 
+	  (1. - exp(-(1. - beta) * x * tg->dtFixed * 
+		    supp_data.TimeUnits / tstar)); // Eq(39)
 
 	// Make the random number.
 	r_float = float(rand())/float(RAND_MAX);
@@ -171,17 +182,17 @@ int ActiveParticleType_SpringelHernquist::EvaluateFormation
 	 * ====================================================================
 	 */
 
-	 starfraction = min(SpringelHernquistMinimumMass / bmass, 0.5);
+	starfraction = min(MinimumMass / bmass, 0.5);
 
-    ActiveParticleType_SpringelHernquist *np = new ActiveParticleType_SpringelHernquist();
-    supp_data.NewParticles[supp_data.NumberOfNewParticles++] = np;
-    //fprintf(stderr, "G_APH: Creating !\n");
+	ActiveParticleType_SpringelHernquist *np = new ActiveParticleType_SpringelHernquist();
+	supp_data.NewParticles[supp_data.NumberOfNewParticles++] = np;
+	//fprintf(stderr, "G_APH: Creating !\n");
 
-    // Give the star mass and then remove the same from the grid.
+	// Give the star mass and then remove the same from the grid.
 	np->Mass = starfraction * density[index];
 	density[index] -= starfraction * density[index];
 
-	np->type = SpringelHernquist;
+	np->type = PARTICLE_TYPE_STAR;
 	np->BirthTime = tg->Time;
 	
 	np->pos[0] = tg->CellLeftEdge[0][i] + 0.5*tg->CellWidth[0][i];
@@ -195,11 +206,11 @@ int ActiveParticleType_SpringelHernquist::EvaluateFormation
 	*/
 
 
-	double *tvel = tg->AveragedVelocityAtCell(index, supp_data.DensNum,
-					       supp_data.Vel1Num);
-    np->vel[0] = tvel[0];
-    np->vel[1] = tvel[1];
-    np->vel[2] = tvel[2];
+	float *tvel = tg->AveragedVelocityAtCell(index, supp_data.DensNum,
+						 supp_data.Vel1Num);
+	np->vel[0] = tvel[0];
+	np->vel[1] = tvel[1];
+	np->vel[2] = tvel[2];
 
 	/* Set the metallicity */
 
@@ -233,23 +244,21 @@ void ActiveParticleType_SpringelHernquist::DescribeSupplementalData
 
 class SpringelHernquistParticleBufferHandler : public ParticleBufferHandler
 {
-  public:
-    SpringelHernquistParticleBufferHandler(int NumberOfParticles) { }
+public:
+  SpringelHernquistParticleBufferHandler(int NumberOfParticles) { }
 };
 
 ParticleBufferHandler *ActiveParticleType_SpringelHernquist::AllocateBuffers(int NumberOfParticles)
 {
-    SpringelHernquistParticleBufferHandler *handler = new SpringelHernquistParticleBufferHandler(NumberOfParticles);
-    return handler;
+  SpringelHernquistParticleBufferHandler *handler = new SpringelHernquistParticleBufferHandler(NumberOfParticles);
+  return handler;
 }
 
 namespace {
-    ActiveParticleType_info *SampleInfo = new ActiveParticleType_info(
-            "SpringelHernquist", (&ActiveParticleType_SpringelHernquist::EvaluateFormation),
-	    (&ActiveParticleType_SpringelHernquist::DescribeSupplementalData),
-	    (&ActiveParticleType_SpringelHernquist::AllocateBuffers),
-	    (&ActiveParticleType_SpringelHernquist::InitializeParticleType),
-	    (&ActiveParticleType_SpringelHernquist::EvaluateFeedback );
-
-
+  ActiveParticleType_info *SampleInfo = new ActiveParticleType_info
+    ("SpringelHernquist", (&ActiveParticleType_SpringelHernquist::EvaluateFormation),
+     (&ActiveParticleType_SpringelHernquist::DescribeSupplementalData),
+     (&ActiveParticleType_SpringelHernquist::AllocateBuffers),
+     (&ActiveParticleType_SpringelHernquist::InitializeParticleType),
+     (&ActiveParticleType_SpringelHernquist::EvaluateFeedback ));
 }
